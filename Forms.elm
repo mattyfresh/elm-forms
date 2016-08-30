55b04e033de6f77a2f1de2ff28811508abc15655
mattyfresh <mpadich@elitedaily.com>
2016-08-29 20:26:14 -0400

    add next stage of form submission

--- Forms.elm
+++ Forms.elm
@@ -3,19 +3,26 @@ module MainForm exposing (..)
 import Html exposing (..)
 import Html.App exposing (beginnerProgram)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput, onSubmit)
+import Html.Events exposing (onInput, onSubmit, onClick)
 import Zipcode
 
 
 type alias Model =
     { zipcode : Maybe String
     , zipcodeEntry : String
+    , departmentType : Maybe DepartmentType
     }
+
+
+type DepartmentType
+    = PoliceDepartment
+    | SheriffDepartment
 
 
 type Msg
     = ZipcodeChanged String
     | SubmitZipcode
+    | ChooseDepartmentType DepartmentType
 
 
 
@@ -33,6 +40,9 @@ update msg model =
             else
                 model
 
+        ChooseDepartmentType departmentType ->
+            { model | departmentType = Just departmentType }
+
 
 
 -- model
@@ -42,6 +52,14 @@ initialModel : Model
 initialModel =
     { zipcode = Nothing
     , zipcodeEntry = ""
+    , departmentType = Nothing
+    }
+
+
+debuggingModel : Model
+debuggingModel =
+    { initialModel
+        | zipcode = Just "11211"
     }
 
 
@@ -49,13 +67,13 @@ initialModel =
 -- view
 
 
-view : Model -> Html Msg
-view model =
+zipcodeForm : String -> Html Msg
+zipcodeForm zipcodeEntry =
     let
         zipcodeIsValid =
-            Zipcode.isValid model.zipcodeEntry
+            Zipcode.isValid zipcodeEntry
     in
-        Html.form [ class "pure-form pure-form-aligned", onSubmit SubmitZipcode ]
+        Html.div [ class "pure-form pure-form-aligned", onSubmit SubmitZipcode ]
             [ fieldset []
                 [ div [ class "pure-control-group" ]
                     [ label [ for "zipcode" ] [ text "Zip Code" ]
@@ -86,10 +104,49 @@ view model =
             ]
 
 
+departmentTypeForm : Html Msg
+departmentTypeForm =
+    Html.div [ class "pure-form" ]
+        [ h1 [ style [ ( "fontFamily", "monospace" ) ] ] [ text "Which kind of authorities exist in your area?" ]
+        , button
+            [ type' "submit"
+            , style [ ( "padding", "20px" ), ( "display", "block" ), ( "margin", "20px auto" ) ]
+            , classList [ ( "pure-button", True ) ]
+            , onClick (ChooseDepartmentType PoliceDepartment)
+            ]
+            [ text "Police Department" ]
+        , button
+            [ type' "submit"
+            , style [ ( "padding", "20px" ), ( "display", "block" ), ( "margin", "20px auto" ) ]
+            , classList [ ( "pure-button", True ) ]
+            , onClick (ChooseDepartmentType SheriffDepartment)
+            ]
+            [ text "Sheriff Department" ]
+        ]
+
+
+view : Model -> Html Msg
+view model =
+    case model.zipcode of
+        Nothing ->
+            zipcodeForm model.zipcodeEntry
+
+        Just zipcode ->
+            case model.departmentType of
+                Nothing ->
+                    departmentTypeForm
+
+                Just PoliceDepartment ->
+                    text "POLICE!!!"
+
+                Just SheriffDepartment ->
+                    text "Sheriff!!!"
+
+
 main : Program Never
 main =
     Html.App.beginnerProgram
-        { model = initialModel
+        { model = debuggingModel
         , view = view
         , update = update
         }
